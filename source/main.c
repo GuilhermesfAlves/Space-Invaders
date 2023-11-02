1a9d1c54791e2199d1734f83049a6f1a97b0399c
GuilhermesfAlves <guilhermesfalves17@gmail.com>
2023-11-02 20:13:27 -0300

    01/11

--- source/main.c
+++ source/main.c
@@ -17,6 +17,12 @@
 /*     /\_____\ \_\ \_\ \___/ \ \__/.\_\ \___,_\ \____\\ \_\\/\____/*/
 /*     \/_____/\/_/\/_/\/__/   \/__/\/_/\/__,_ /\/____/ \/_/ \/___/ */
                                                                  
+typedef struct{
+    ALLEGRO_COLOR primary;
+    ALLEGRO_COLOR secondary;
+    ALLEGRO_COLOR back_theme;
+}theme;
+
 void START_ALERT(ALLEGRO_FONT* font, int frame, int move){
     if ((!move) && ((frame < 40) || ((frame > 80) && (frame < 120)) || ((frame > 160) && (frame < 200))))
         al_draw_text(font, al_map_rgb(255, 255, 255), 960, 960 - move, ALLEGRO_ALIGN_CENTRE, "Press SPACE to Start!");
@@ -31,9 +37,15 @@ int main(){
     ALLEGRO_EVENT event;
     ALLEGRO_TIMER* timer = al_create_timer(1.0 / 60.0);
     ALLEGRO_EVENT_QUEUE* queue = al_create_event_queue();
-    ALLEGRO_DISPLAY* disp = al_create_display(1920, 1080);
+    ALLEGRO_DISPLAY_MODE disp_data;
+    al_get_display_mode(al_get_num_video_adapters() - 1, &disp_data);
+    ALLEGRO_DISPLAY* disp = al_create_display(disp_data.width, disp_data.height);
     ALLEGRO_FONT* font = al_create_builtin_font();
-    ALLEGRO_BITMAP *logo = al_load_bitmap("SPACE_INVADERS_LOGO.bmp");
+    ALLEGRO_BITMAP *logo = al_load_bitmap("img/SPACE_INVADERS_LOGO.bmp");
+    ALLEGRO_BITMAP *logo_redimens = al_create_bitmap(disp_data.width*0.359895833, disp_data.width*0.33365937);
+    al_set_target_bitmap(logo_redimens);
+    al_draw_scaled_bitmap(logo, 0, 0, al_get_bitmap_width(logo), al_get_bitmap_height(logo), 0, 0, disp_data.width*0.359895833, disp_data.width*0.33365937, 0);
+    al_set_target_bitmap(al_get_backbuffer(disp));
     int frame = 0;
     int move = 0;
     char start = 0;
@@ -59,12 +71,12 @@ int main(){
             exit = 1;
             break;
         }
-        if (move == 720)
+        if (move == 760)
             break;
 
         if ((event.type == ALLEGRO_EVENT_TIMER) && (al_is_event_queue_empty(queue))){
             al_clear_to_color(al_map_rgb(54, 54, 54));
-            al_draw_bitmap(logo, 645, 248 - 140 - move, 0);
+            al_draw_bitmap(logo_redimens, (disp_data.width - al_get_bitmap_width(logo_redimens))/2, 248 - 140 - move, 0);
             START_ALERT(font, frame, move);
             al_flip_display();
         }
